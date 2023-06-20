eb7f0ccd0c0a1a62c6d69ffcdc7a17b02b87de57
Nadezhda <melnik.nadezhdayu@yandex.ru>
2023-06-20 11:58:26 +0300

    2.2

---
+++ java_projects/program.java
@@ -0,0 +1,8 @@
+// package java_projects;
+
+public class program {
+    public static void main(String[] args) {
+        System.out.printLn("world");
+    }
+    
+}
---
+++ pr2.2.java
@@ -0,0 +1,21 @@
+public class pr2.2 {
+    public static void main(String[] args) {
+        int [] mas = {1, 3, 4, 2};
+ 
+        boolean isSorted = false;
+        int buf;
+        while(!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < mas.length-1; i++) {
+                if(mas[i] > mas[i+1]){
+                    isSorted = false;
+ 
+                    buf = mas[i];
+                    mas[i] = mas[i+1];
+                    mas[i+1] = buf;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(mas));
+    }
+}
