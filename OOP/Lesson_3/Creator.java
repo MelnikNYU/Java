aea18aaa702ed0fefb0ee962595e4b8b4c7a2408
Nadezhda <melnik.nadezhdayu@yandex.ru>
2023-07-21 00:03:30 +0300

    m

---
+++ OOP/Lesson_3/Creator.java
@@ -0,0 +1,23 @@
+package OOP.Lesson_3;
+
+import java.io.FileWriter;
+// import java.io.IOException;
+
+// public class Creator {
+//     public static void main(String[] args) {
+        
+//         try(FileWriter writer = new FileWriter("doc3.txt", false))
+//         {
+//             String text = "Hello File!";
+//             writer.write(text);
+//             writer.append('\n');
+//             writer.append('E');
+             
+//             writer.flush();
+//         }
+//         catch(IOException ex){
+             
+//             System.out.println(ex.getMessage());
+//         } 
+//     } 
+// }
